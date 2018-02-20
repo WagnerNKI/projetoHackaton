3af882157c9139429de543899cf582ebec8cb877
WAGNER NORIO KANASHIRO ISHIKAWA <32658368860@jpmorgan.sp>
2018-02-20 19:49:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var five = require("johnny-five");
+
+var board = new five.Board({ 
+    port: "COM3" 
+});
+
+board.on("ready", function() {
+
+  button7 = new five.Button({
+    pin: 7,
+    isPullup: true
+  });
+
+  button6 = new five.Button({
+    pin: 6,
+    isPullup: true
+  });
+
+
+  button5 = new five.Button({
+    pin: 5,
+    isPullup: true
+  });
+
+  button7.on("down", function(value) {
+        console.log("7");
+  });
+
+  button6.on("down", function(value) {
+        console.log("6");
+  });
+
+
+  button5.on("down", function(value) {
+        console.log("5");
+  });
+
+});
