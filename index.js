d901f4dd4dd9d4853601992d3db56b9bd4cb3c26
WAGNER NORIO KANASHIRO ISHIKAWA <32658368860@jpmorgan.sp>
2018-02-20 21:47:02 -0300

    butoes

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 var five = require("johnny-five");
 
-var board = new five.Board({ 
-    port: "COM3" 
+var board = new five.Board({
+    port: "COM3"
 });
 
-board.on("ready", function() {
+var buttonPressed;
 
-  button7 = new five.Button({
-    pin: 7,
-    isPullup: true
-  });
+board.on("ready", function () {
 
-  button6 = new five.Button({
-    pin: 6,
-    isPullup: true
-  });
+    button7 = new five.Button({
+        pin: 7,
+        isPullup: true
+    });
+
+    button6 = new five.Button({
+        pin: 6,
+        isPullup: true
+    });
 
 
-  button5 = new five.Button({
-    pin: 5,
-    isPullup: true
-  });
+    button5 = new five.Button({
+        pin: 5,
+        isPullup: true
+    });
 
-  button7.on("down", function(value) {
+    button7.on("down", function () {
         console.log("7");
-  });
+        buttonPressed = 7;
+    });
 
-  button6.on("down", function(value) {
+    button6.on("down", function () {
         console.log("6");
-  });
+        buttonPressed = 6;
+    });
 
 
-  button5.on("down", function(value) {
+    button5.on("down", function () {
         console.log("5");
-  });
+        buttonPressed = 5;
+    });
 
 });
