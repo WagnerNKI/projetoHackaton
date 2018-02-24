640ac75696fc3e5291cd7360bcc009742d2ccb17
WAGNER NORIO KANASHIRO ISHIKAWA <32658368860@jpmorgan.sp>
2018-02-23 21:58:01 -0300

    LCD autoscroll

--- index.js
+++ index.js
@@ -1,72 +1,115 @@
 var five = require("johnny-five");
-const question = require('./questions.json');
+const question = require('./questions');
+const scroll = require("lcd-scrolling");
+const _ = require("lodash");
+var lcd;
 
 var board = new five.Board({
     port: "COM3",
-    repl: false
+    repl: false,
 });
+var prefixos = [
+    "A",
+    "B",
+    "C"
+]
 
-var buttonPressed;
+var givenAnswer;
 var rightAnswer = question.rightAnswer;
-console.log(question.question);
-console.log("A) " + question.alternative1);
-console.log("B) " + question.alternative2);
-console.log("C) " + question.alternative3);
+var questionShow = question.question;
+var alternativas  = _.chain(prefixos)
+    .zip(question.alternativas)
+    .map(function(prefixoComAlternativa) {
+        return prefixoComAlternativa.join(") ")
+    }).value();
+
+console.log(questionShow);
+console.log(alternative1);
+console.log(alternative2);
+console.log(alternative3);
+console.log("Resposta:");
 
 board.on("ready", function () {
-
-    var lcd = new five.LCD({
-        pins: [7, 8, 9, 10, 11, 12],
-        backlight: 13,
-    });
-
-    lcd.cursor(0,0);
-    lcd.autoscroll().print("Bloop").print("asdfasdfadsfsaadfasdf");
-    lcd.cursor(1,0);
-    lcd.autoscroll().print("Bloop").print("asdfasdfadsfsaadfasdf");
-
-    button7 = new five.Button({
-        pin: 7,
-        isPullup: true
-    });
 
     button6 = new five.Button({
         pin: 6,
         isPullup: true
     });
 
-
     button5 = new five.Button({
         pin: 5,
         isPullup: true
     });
 
-    button7.on("down", function () {
-        console.log(question.alternative1);
-        buttonPressed = question.alternative1;
-        isRightAnswer();
+    button4 = new five.Button({
+        pin: 4,
+        isPullup: true
     });
 
     button6.on("down", function () {
-        console.log(question.alternative2);
-        buttonPressed = question.alternative2;
+        console.log(alternative1);
+        givenAnswer = alternative1;
         isRightAnswer();
     });
-
 
     button5.on("down", function () {
-        console.log(question.alternative3);
-        buttonPressed = question.alternative3;
+        console.log(alternative2);
+        givenAnswer = alternative2;
         isRightAnswer();
     });
 
+
+    button4.on("down", function () {
+        console.log(alternative3);
+        givenAnswer = alternative3;
+        isRightAnswer();
+    });
+
+    lcd = new five.LCD({
+        pins: [7, 8, 9, 10, 11, 12],
+        rows: 2,
+        cols: 16
+    });
+
+    scroll.setup({
+        lcd: lcd,
+    });
+
+    mostrarAlternativas();
 });
 
+function mostrarAlternativas() {
+    scroll.line(0, questionShow);
+    var textoAlternativas = alternativas.join(" ");
+    scroll.line(1, textoAlternativas);
+}
+
+
 function isRightAnswer() {
-    if (buttonPressed == rightAnswer) {
-        console.log("Resposta correta")
+
+    var answer;
+    switch (givenAnswer) {
+
+        case alternative1:
+            answer = question.alternative1;
+            break;
+
+        case alternative2:
+            answer = question.alternative2;
+            break;
+
+        case alternative3:
+            answer = question.alternative3;
+            break;
+    };
+
+    if (answer == rightAnswer) {
+        console.log("Resposta correta");
+        scroll.line(1, "Resposta correta");
     }
     else {
-        console.log("Resposta errada")
+        console.log("Resposta errada");
+        scroll.line(1, "Resposta errada");
+        setTimeout(mostrarAlternativas, 2000);
     }
 }
